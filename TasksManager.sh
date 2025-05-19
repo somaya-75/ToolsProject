451cdbf7046a459180705cfc335bb3e514639618
somaya-75 <s.o.s.r5050@gmail.com>
2025-05-20 00:32:00 +0300

    final version with tested features

--- TasksManager.sh
+++ TasksManager.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#Function 1: Add Task          somaya    44510533
+#Function 1: Add Task         name:somaya     id:44510533
 add_task() {
     echo "Enter task name:"
     read task_name
@@ -11,49 +11,41 @@ add_task() {
     echo "Enter priority (High / Medium / Low):"
     read priority
 
-    # check date format
+    #Check if date format is valid
     if ! date -d "$due_date" &>/dev/null; then
         echo "Invalid date format. Use YYYY-MM-DD."
         return
     fi
 
+    # Save task to file
     echo "$task_name | $due_date | $priority" >> "tasks.txt"
     echo "Task added successfully!"
 }
 
-# function 2      ruyuf        id
 
-#!/bin/bash
-
-# Function to list tasks from tasks.txt
+#Function 2: list tasks      name:ruyuf      id:44510064
 list_tasks() {
+  # Check if tasks file exists
   if [[ ! -f tasks.txt ]]; then
     echo "Error: tasks.txt not found!"
     return 1
   fi
+  # Display tasks
   cat tasks.txt
 }
 
-# Call the function
-list_tasks
 
-
-
-
-
-
-
-
-#Function 3: Remind Tasks                somaya     44510533
+#Function 3: Remind Tasks             name:somaya      id:44510533
 remind_tasks() {
     echo "Checking for upcoming tasks..."
-    today=$(date +%s)
+    today=$(date +%s) # Get current date in seconds
 
+    # Make sure the tasks file exists
     if [ ! -f "tasks.txt" ]; then
         echo "tasks.txt not found."
         return
     fi
-
+    # Read each line from tasks.txt
     while IFS="|" read -r name date priority; do
         task_date=$(date -d "$date" +%s 2>/dev/null)
 
@@ -61,9 +53,10 @@ remind_tasks() {
         if [ -z "$task_date" ]; then
             continue
         fi
-
+       # Calculate how many days left
         days_left=$(( (task_date - today) / 86400 ))
 
+# Show reminder if task is due in 2 days or less
         if [ "$days_left" -le 2 ] && [ "$days_left" -ge 0 ]; then
             echo -e "\e[31mReminder:\e[0m \"$name\" is due in $days_left day(s)! (Priority: $priority)"
         fi
@@ -74,11 +67,14 @@ case "$1" in
     add)
         add_task
         ;;
+    list)
+        list_tasks
+        ;;
     remind)
         remind_tasks
         ;;
     *)
-        echo "Use: $0 {add | remind}"
+        echo "Use: $0 {add | list | remind}"
         ;;
 esac
 
